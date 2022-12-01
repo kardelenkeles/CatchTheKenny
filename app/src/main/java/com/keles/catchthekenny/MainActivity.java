b079ad040a67ac6c0ecf9e9a1b2b2f2cd50a7cb9
kardelenkeles <kar_di44@hotmail.com>
2022-12-01 21:19:56 +0300

    increaseScore function created

--- app/src/main/java/com/keles/catchthekenny/MainActivity.java
+++ app/src/main/java/com/keles/catchthekenny/MainActivity.java
@@ -3,12 +3,29 @@ package com.keles.catchthekenny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView scoreText;
+    TextView timeText;
+    int score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeText = (TextView) findViewById(R.id.timeText);
+        scoreText = (TextView) findViewById(R.id.scoreText);
+
+        score = 0;
     }
+
+    public void increaseScore(View view){
+        score ++;
+        scoreText.setText("Score: " + score);
+
+    }
+
+
 }
